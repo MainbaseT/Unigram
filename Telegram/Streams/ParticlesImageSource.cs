2fe4c3d50825ff1fa5cbe03d4c673905a2c21fa7
Fela <frayx@live.it>
2025-06-17 13:13:04 +0400

    Generate unique hash for particles

--- Telegram/Streams/ParticlesImageSource.cs
+++ Telegram/Streams/ParticlesImageSource.cs
@@ -4,6 +4,7 @@
 // Distributed under the GNU General Public License v3.0. (See accompanying
 // file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
 //
+using System;
 using Windows.UI;
 
 namespace Telegram.Streams
@@ -28,6 +29,11 @@ namespace Telegram.Streams
             Foreground = foreground;
             Background = Colors.Transparent;
             IsText = true;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Foreground, Background);
         }
 
         public override string FilePath => string.Empty;
