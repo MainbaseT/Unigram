MainbaseT/Unigram

commit 3a5d99d4121cff1ab751ce39f3161234bb4e643c

Save state on suspending

Telegram/Navigation/Services/NavigationService.cs
@@ -17,6 +17,7 @@ using Telegram.ViewModels.Gallery;
 using Telegram.Views;
 using Telegram.Views.Popups;
 using Telegram.Views.Settings;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -229,6 +230,16 @@ namespace Telegram.Navigation.Services
             FrameFacade = new FrameFacade(this, frame, id);
             FrameFacade.Navigating += OnNavigating;
             FrameFacade.Navigated += OnNavigated;
+
+            if (frame != null)
+            {
+                Application.Current.Suspending += OnSuspending;
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            Suspend();
         }
 
         private void OnNavigating(object sender, NavigatingEventArgs e)

commit 9a08a8f68eaf1a045ceb9594f4d000194274788f

Fix leak on account switch Introduced in 3a5d99d4121cff1ab751ce39f3161234bb4e643c

Telegram/Navigation/Services/NavigationService.cs
@@ -30,6 +30,9 @@ namespace Telegram.Navigation.Services
 {
     public interface INavigationService
     {
+        void Connect();
+        void Disconnect();
+
         void GoBack(NavigationState state = null, NavigationTransitionInfo infoOverride = null);
         void GoBackAt(int index, bool back = true);
         void GoForward();
@@ -232,11 +235,29 @@ namespace Telegram.Navigation.Services
             FrameFacade = new FrameFacade(this, frame, id);
             FrameFacade.Navigating += OnNavigating;
             FrameFacade.Navigated += OnNavigated;
+        }
 
-            if (frame != null)
+        private bool _connected;
+
+        public void Connect()
+        {
+            if (_connected)
             {
-                Application.Current.Resuming += OnResuming;
-                Application.Current.Suspending += OnSuspending;
+                return;
+            }
+
+            _connected = true;
+            Application.Current.Resuming += OnResuming;
+            Application.Current.Suspending += OnSuspending;
+        }
+
+        public void Disconnect()
+        {
+            if (_connected)
+            {
+                _connected = false;
+                Application.Current.Resuming -= OnResuming;
+                Application.Current.Suspending -= OnSuspending;
             }
         }
 

Telegram/Navigation/Services/NavigationServiceList.cs
@@ -4,14 +4,44 @@
 // Distributed under the GNU General Public License v3.0. (See accompanying
 // file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
 //
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Telegram.Navigation.Services
 {
-    public partial class NavigationServiceList : List<INavigationService>
+    public partial class NavigationServiceList : IReadOnlyList<INavigationService>
     {
+        private readonly List<INavigationService> _items = new();
+
+        public INavigationService this[int index] => _items[index];
+
+        public int Count => _items.Count;
+
+        public void Add(INavigationService navigationService)
+        {
+            _items.Add(navigationService);
+            navigationService.Connect();
+        }
+
+        public void Remove(INavigationService navigationService)
+        {
+            _items.Remove(navigationService);
+            navigationService.Disconnect();
+        }
+
+        public void Clear()
+        {
+            foreach (var service in _items)
+            {
+                service.Disconnect();
+            }
+
+            _items.Clear();
+        }
+
         public INavigationService GetByFrameId(string frameId) => this.FirstOrDefault(x => x.FrameFacade.FrameId == frameId);
+
         public INavigationService RemoveByFrameId(string frameId)
         {
             var service = GetByFrameId(frameId);
@@ -33,6 +63,16 @@ namespace Telegram.Navigation.Services
                     Remove(service);
                 }
             }
+        }
+
+        public IEnumerator<INavigationService> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }